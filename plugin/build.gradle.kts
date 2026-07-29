25b106286fd433c7cd1ef8e321ff831eb8170068
FletchMcKee <cfmckee.dev@gmail.com>
2026-07-29 15:28:55 -0500

    Update compileClasspath to target jvm version 17

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -34,9 +34,13 @@ tasks.withType<Test>().configureEach {
   useJUnitPlatform()
 }
 
-// JUnit 6 requires a minimum jdk of 17. Overriding the test classpath attributes seems like the easiest
-// way to use JUnit 6 for testing while allowing the plugin to support jdk 11.
 configurations {
+  compileClasspath {
+    attributes {
+      attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
+    }
+  }
+
   testCompileClasspath {
     attributes {
       attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
